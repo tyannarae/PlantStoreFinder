b677e4f90106573d7e99673010a2e831f6260400
tyannarae <tyannarae1994@gmail.com>
2021-03-09 12:21:10 -0600

    formatted the context file with prettier

--- src/context/pages/cityPage.ts
+++ src/context/pages/cityPage.ts
@@ -1,20 +1,21 @@
-import { createContext, useContext } from 'react'
-import {Store, Stores} from '../../database/stores'
+import { createContext, useContext } from "react";
+import { Store, Stores } from "../../database/stores";
 
 export type CityPageContextType = {
-    selectedStore: Store, 
-    setSelectedStore: (chosenStore: Store) => void,
-    isModalOpen: boolean,
-    setModalOpen: (isOpen: boolean) => void,
-    stores: Array<Store>,
-}
+  selectedStore: Store;
+  setSelectedStore: (chosenStore: Store) => void;
+  isModalOpen: boolean;
+  setModalOpen: (isOpen: boolean) => void;
+  stores: Array<Store>;
+};
 
 export const CityPageContext = createContext<CityPageContextType>({
-    selectedStore: Stores[0],
-    setSelectedStore: chosenStore => console.warn('no setSelectedStore context provider'),
-    isModalOpen: false,
-    setModalOpen: isOpen => console.warn('modal no context provider'),
-    stores: Stores,
-})
+  selectedStore: Stores[0],
+  setSelectedStore: (chosenStore) =>
+    console.warn("no setSelectedStore context provider"),
+  isModalOpen: false,
+  setModalOpen: (isOpen) => console.warn("modal no context provider"),
+  stores: Stores,
+});
 
 export const useSearchContext = () => useContext(CityPageContext);
