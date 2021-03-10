ff696ec0bf8d6c0b5c11d2d1c74834fbc5c939ab
tyannarae <tyannarae1994@gmail.com>
2021-03-10 12:02:47 -0600

    made code review changes. updated storeLat-Lng to be just lat lng. Also updating the id type to be a string

--- src/database/stores.ts
+++ src/database/stores.ts
@@ -1,10 +1,10 @@
 //this interface is for a single store in the Stores database
 export interface Store {
-    id: number,
+    id: string,
     city: string,
     state: string,
-    storeLat: number,
-    storeLng: number,
+    lat: number,
+    lng: number,
     bussinessName: string,
     website: string,
     address: string,
@@ -38,11 +38,11 @@ export const Stores: Array<City> = [
     },
     stores: [
       {
-        id: 1,
+        id: 'eas1',
         city: "Austin",
         state: "Texas",
-        storeLat: 30.261270,
-        storeLng: -97.704220,
+        lat: 30.261270,
+        lng: -97.704220,
         bussinessName: "East Austin Succulents",
         website: "https://eastaustinsucculents.com/",
         address: "801 Tillery St, Austin, Texas 78702",
@@ -53,11 +53,11 @@ export const Stores: Array<City> = [
         photos: ["https://www.mollyonthemoveblog.com/wp-content/uploads/2016/05/east-austin-succulents.jpg", "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/145499779_2747114298833410_4804550472908776278_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=a26aad&_nc_ohc=m8GKm03jtM8AX9BoqT8&_nc_ht=scontent-dfw5-2.xx&oh=d035251d68c89d247ed4a24064897527&oe=606D4915", "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/132018400_2717071651837675_1105403803744973353_o.jpg?_nc_cat=109&ccb=1-3&_nc_sid=a26aad&_nc_ohc=cC970Oqm4UMAX886NpF&_nc_ht=scontent-dfw5-2.xx&oh=e97c74040dac84243723c2350fe3a228&oe=606B8735"],
       },
       {
-        id: 2,
+        id: 'tgo2',
         city: "Austin",
         state: "Texas",
-        storeLat: 30.234770,
-        storeLng: -97.757650,
+        lat: 30.234770,
+        lng: -97.757650,
         bussinessName: "The Great Outdoors",
         website: "http://gonursery.com/",
         address: "2730 S Congress Ave, Austin, TX 78704",
