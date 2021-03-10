18a2eca9f23575af0cb11af110c85d4e76e80403
tyannarae <tyannarae1994@gmail.com>
2021-03-10 15:16:50 -0600

    installed leaflet map. Just trying to figure out css

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./map.scss";
 
 function Map() {
-  return <div className="map">Map</div>;
+  // const mymap = MapContainer.map('mapid').setView([51.505, -0.09], 13);
+  return (
+    <MapContainer
+      id="mapid"
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default Map;
