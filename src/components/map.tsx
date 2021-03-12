6d0720cd575cca9ffbd360ce5c85f230d6c1b649
tyannarae <tyannarae1994@gmail.com>
2021-03-12 14:10:21 -0600

    added a simply test. Need to work on testing the inner components

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,28 +2,30 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./map.scss";
 
-function Map() {
+export const Map = () => {
   /* once we have merged context and the database I will update this information rather than hard coding it. */
   return (
-    <MapContainer
-      id="mapid"
-      center={[30.2672, -97.7431]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* Marker and Popup will also be a seperate components. I want to keep these here for now so I can reference how to create them once everythig is merged.  */}
-      <Marker position={[30.26127, -97.70422]}>
-        <Popup>East Austin Succulents</Popup>
-      </Marker>
-      <Marker position={[30.23477, -97.75765]}>
-        <Popup>The Great Outdoor Store</Popup>
-      </Marker>
-    </MapContainer>
+    <div data-testid="mapContainer">
+      <MapContainer
+        id="mapid"
+        center={[30.2672, -97.7431]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Marker and Popup will also be a seperate components. I want to keep these here for now so I can reference how to create them once everythig is merged.  */}
+        <Marker position={[30.26127, -97.70422]}>
+          <Popup>East Austin Succulents</Popup>
+        </Marker>
+        <Marker position={[30.23477, -97.75765]}>
+          <Popup>The Great Outdoor Store</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
-}
+};
 
 export default Map;
