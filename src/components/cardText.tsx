e483de619329aad32b0d61317eba1e9a48f8f981
tyannarae <tyannarae1994@gmail.com>
2021-03-15 17:16:13 -0500

    text component is looking good. Going to start working on tests and rendering at different screen sizes next

--- src/components/cardText.tsx
+++ src/components/cardText.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from "react";
 import { Stores, Store } from "../database/stores";
+import fbLogo from "../media/fbLogo.png";
+import igLogo from "../media/igLogo.png";
+import "../components/cardText.scss";
 
 export interface cardTextProps {
   store: Store;
@@ -19,31 +22,59 @@ export const CardText: FunctionComponent<cardTextProps> = (cardTextProps) => {
   } = cardTextProps.store;
   //opens a new tab for the stores website
   return (
-    <div className="card">
-      <div className="card-content ">
-        <div className="tile is-ancestor">
-          <div className="tile is-vertical is-8">
-            <div className="tile">
-              <div className="tile is-parent is-vertical">{bussinessName}</div>
-              <div className="tile is-parent is-vertical">link</div>
+    <div className="card-content ">
+      <div className="tile is-ancestor">
+        <div className="tile is-vertical ">
+          <div className="tile header">
+            <div className="address tile is-parent is-vertical is-8">
+              <div className="businessName"> {bussinessName}</div>
             </div>
-            <div className="tile">
-              <div className="tile is-parent is-vertical">
-                <div>{address}</div>
-              </div>
-              <div className="tile is-parent is-vertical">
+            <div className="tile is-parent is-vertical endItem">
+              <div className="tile">
                 <div
+                  className="linkImgContainer"
                   onClick={(event: any) => {
-                    openWebsite(website);
+                    openWebsite(fbHandle);
                   }}
                 >
-                  Visit Website
+                  <img
+                    className="linkImg tile is-vertical"
+                    alt="link"
+                    src={fbLogo}
+                  />
                 </div>
-                <div>{phone}</div>
+                <div
+                  className="linkImgContainer"
+                  onClick={(event: any) => {
+                    openWebsite(igHandle);
+                  }}
+                >
+                  <img
+                    className="linkImg tile is-vertical"
+                    alt="link"
+                    src={igLogo}
+                  />
+                </div>
               </div>
             </div>
-            <div className=" is-parent tile">{blurb}</div>
           </div>
+
+          <div className="tile">
+            <div className="address tile is-parent is-vertical">
+              <div>{address}</div>
+            </div>
+            <div className="tile is-parent is-vertical endItem">
+              <div
+                onClick={(event: any) => {
+                  openWebsite(website);
+                }}
+              >
+                Visit Website
+              </div>
+              <div>{phone}</div>
+            </div>
+          </div>
+          <div className="is-parent tile">{blurb}</div>
         </div>
       </div>
     </div>
