45096a62ecc6b6cfb9662ae4c39fcf1f5eac9825
tyannarae <tyannarae1994@gmail.com>
2021-03-16 12:04:34 -0500

    moved global vairable to local function

--- src/context/pages/cityPage.ts
+++ src/context/pages/cityPage.ts
@@ -6,8 +6,8 @@ const stores = Stores[0].stores;
 interface StoreIndexMap {   //interface for the map of store indices and their id.
    [key: string]: number //key will be store ID and value will be index
 }
-let storeId: StoreIndexMap = {}; 
 export const mapStoreIdsToIndex =(stores: Array<Store>) =>{
+  let storeId: StoreIndexMap = {}; 
   stores.forEach( 
     (
       store: Store,
