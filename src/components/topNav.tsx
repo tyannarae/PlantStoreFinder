55e04436f8f7fc04659b9eb22cb7808ace027a48
tyannarae <tyannarae1994@gmail.com>
2021-03-24 16:26:27 -0500

    rendering api results that were stached in session storage

--- src/components/topNav.tsx
+++ src/components/topNav.tsx
@@ -1,14 +1,24 @@
-import { FunctionComponent } from "react";
+import react, { FunctionComponent } from "react";
 import { MapCoordinates } from "../database/stores";
-import { weather } from "../database/weatherResults";
+import { getWeather } from "../database/weatherResults";
 const dotenv = require("dotenv"); // gives me access to my env file
 dotenv.config({ debug: process.env.DEBUG });
 export interface TopNavProps {
   seletedCity: MapCoordinates;
 }
 export const TopNav: FunctionComponent<TopNavProps> = (TopNavProps) => {
-  console.log(weather.currently);
-  return <div>Current Temp:</div>;
+  if (!sessionStorage.weather) {
+    //check if weather session storage isnt set yet.
+    getWeather();
+  }
+  var weatherObj = JSON.parse(sessionStorage.getItem("weather") as string);
+  console.log("kjdkjsdkjh", weatherObj);
+  return (
+    <div className="navbar">
+      <div className="navbar-item">Current Temp:</div>
+      <div className="navbar-item">{weatherObj.apparentTemperature}</div>
+    </div>
+  );
 };
 
 export default TopNav;
--- src/database/weatherResults.ts
+++ src/database/weatherResults.ts
@@ -1,25 +1,22 @@
 const dotenv = require("dotenv");
 dotenv.config({ debug: process.env.DEBUG });
 export const collectionName = "weather";
-
 export const env = {
   port: process.env.DARK_SKY_API_KEY,
 };
 console.log("env.port " + env.port);
-
-export let weather: any = getWeather();
+let weather: any;
 
 
 const setSessionStorage =() =>{
   window.sessionStorage.setItem(collectionName, JSON.stringify(weather.currently)); 
-  console.log('!!!',sessionStorage, '!!!')
   }
 
 // how the user can retreive tempature data for the city 
-async function getWeather() {
+export async function getWeather() {
   const darkSkyUrl =
     "https://api.darksky.net/forecast/" +
-+
+
     "/" +
     "30.234770" +
     "," +
