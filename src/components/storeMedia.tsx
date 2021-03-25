237255dd37d8404eef5f01f24db734090a996c0b
tyannarae <tyannarae1994@gmail.com>
2021-03-25 16:11:04 -0500

    name changed an interface

--- src/components/storeMedia.tsx
+++ src/components/storeMedia.tsx
@@ -2,13 +2,13 @@ import React, { FunctionComponent, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./storeMedia.scss";
 
-export interface StoreMediaDetailsProps {
+export interface StoreMediaProps {
   photos: Array<string>;
 }
-export const StoreMedia: FunctionComponent<StoreMediaDetailsProps> = (
-  StoreMediaDetailsProps
+export const StoreMedia: FunctionComponent<StoreMediaProps> = (
+  StoreMediaProps
 ) => {
-  const { photos } = StoreMediaDetailsProps;
+  const { photos } = StoreMediaProps;
   const [photoIndex, setPhoto] = useState(0);
 
   //takes the id of the store that was clicked and updates the selectedStore
