0d055f6c73d0f8f6607865ffe8511639dd18b08a
tyannarae <tyannarae1994@gmail.com>
2021-03-25 15:50:11 -0500

    removed component that should not be in this apart of this branch

--- src/components/StoreCard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StoreDetails } from "./storeDetails";
-import { StoreMedia } from "./storeMedia";
-export const StoreCard = () => {
-  return <div></div>;
-};
-
-export default StoreCard;
