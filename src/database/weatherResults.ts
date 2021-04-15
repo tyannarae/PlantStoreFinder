8b38eaa6dbc9c21fd733de55f07460bda264a9a8
tyannarae <tyannarae1994@gmail.com>
2021-04-15 12:53:27 -0500

    using database location corrdinates. updated type of lat and lng to string

--- src/database/weatherResults.ts
+++ src/database/weatherResults.ts
@@ -1,15 +1,17 @@
+import {Stores} from '../database/stores'
 export let temp: string;
 let weather:any;
 
-
+const coordinates = Stores[0].mapCoordinates;
 // how the user can retreive tempature data for the city 
 export async function getWeather() {
   const darkSkyUrl =
     "https://api.darksky.net/forecast/8ae444357252f2b0ac14bab6b546d9ed/" +
-    "30.234770,"+
-    "-97.757650";
+    coordinates.lat+","+
+    coordinates.lng;
   const response = await fetch(darkSkyUrl);
   const body = await response.json();
+  console.log(darkSkyUrl)
   temp = JSON.stringify(body.currently.apparentTemperature)
 }
 getWeather();
