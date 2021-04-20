7d1643261c8224bf66bcb4a4d12a3aa7a81dca15
tyannarae <tyannarae1994@gmail.com>
2021-04-20 15:43:24 -0500

    added testing for modal component

--- src/components/mediaModal.test.tsx
+++ src/components/mediaModal.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import { CityPageContext, mapStoreIdsToIndex } from "../context/pages/cityPage";
 import { MediaModal } from "./mediaModal";
 
 describe("StoreMedia", () => {
@@ -23,6 +24,17 @@ describe("StoreMedia", () => {
       "https://rootedinmoss.files.wordpress.com/2015/11/img_2245.jpg",
     ],
   };
+  const fakeData = [
+    {
+      city: "Austin",
+      state: "Texas",
+      mapCoordinates: {
+        lat: 30.2672,
+        lng: -97.7431,
+      },
+      stores: [fakeStore],
+    },
+  ];
   const photos = fakeStore.photos;
   it("Renders with the first photo in the array visible", () => {
     render(<MediaModal photos={photos} />);
@@ -37,5 +49,33 @@ describe("StoreMedia", () => {
     render(<MediaModal photos={photos} />);
     fireEvent.click(screen.getByTestId("modalImgForward"));
     expect(screen.getByTestId("modalImg").src).toEqual(photos[1]);
+  });
+  it("renders with backward button", () => {
+    render(<MediaModal photos={photos} />);
+    expect(screen.getByText("<")).toBeDefined();
+  });
+  it("renders with forward button", () => {
+    render(<MediaModal photos={photos} />);
+    expect(screen.getByText(">")).toBeDefined();
+  });
+  it("renders with exit button and closes modal onClick", () => {
+    const contextValue = {
+      city: fakeData[0],
+      isModalOpen: true,
+      seletedCity: fakeData[0].mapCoordinates,
+      selectedStore: fakeStore,
+      setSelectedStore: jest.fn(),
+      stores: fakeData[0].stores,
+      setModalOpen: jest.fn(),
+      storeIdToIndexMap: mapStoreIdsToIndex(fakeData[0].stores),
+    };
+    render(
+      <CityPageContext.Provider value={contextValue}>
+        <MediaModal photos={photos} />
+      </CityPageContext.Provider>
+    );
+    expect(screen.getByText("X")).toBeDefined();
+    fireEvent.click(screen.getByTestId("exitButton"));
+    expect(contextValue.setModalOpen).toHaveBeenCalledWith(false);
   });
 });
